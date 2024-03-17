1377716de628876ab1722d8f58852933902c8c5c
Andreas Dreyer Hysing <andreashysing@gmail.com>
2024-03-17 21:42:57 +0100

    curated new datasets

--- src/filter_byus.sh
+++ src/filter_byus.sh
@@ -1,5 +1,5 @@
 echo "area,distance,first_registration,fuel,price,search,title,url,year" > drawer/vestland.CSV;
 cat *.csv | \
     grep 'finn.no' | \
-    grep -E '(5257 Kokstad| Blomsterdalen | Bønes| Sandsli)' | \
+    grep -E '(5257 Kokstad| Blomsterdalen | Bønes| Sandsli| Rådal)' | \
     sort -u -r >> drawer/vestland.CSV;
